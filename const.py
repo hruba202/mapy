927fddedc6c2c38f4aea96dcbd06a0c4fe1beb37
h202 <131245859+hruba202@users.noreply.github.com>
2026-07-29 20:16:38 +0200

    Delete const.py

--- const.py
+++
@@ -1,9 +0,0 @@
-#const.py – Konstanty a API endpointy:
-python
-DOMAIN = "mapycz_rest"
-
-CONF_API_KEY = "api_key"
-CONF_GEO_QUERY = "geo_query"
-CONF_ROUTE_START = "route_start"
-CONF_ROUTE_END = "route_end"
-CONF_ROUTE_MODE = "route_mode"
